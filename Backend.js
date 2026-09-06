2f0ea3d7b51420742721f5b06d4c7a7320b63637
pogaming4267 <pogaming921@gmail.com>
2026-09-06 05:31:59 +0300

    websoocket established

--- Backend.js
+++ Backend.js
@@ -1,10 +1,15 @@
 
 function myFunction() {
     document.getElementById("myButton").innerHTML ="Clicked!";
+    socket.send('Alert');
     setTimeout(function() {
         document.getElementById("myButton").innerHTML = "Press Me";
     }, 1000);
 }
 
 document.getElementById("myButton").addEventListener("click", myFunction);
+const socket = new WebSocket('ws://localhost:9002');
+socket.onopen = function() {
+    console.log('WebSocket connection established');
+}
 
